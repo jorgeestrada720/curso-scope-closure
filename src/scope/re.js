12da1ca3dfc84553174a94a0558121b9fb68f83f
Jorge Estrada <ejorge720@gmail.com>
2022-07-25 12:38:15 -0600

    Reasignacion y Redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,31 @@
+var firstName; //Undefined
+firstName = 'Jorge';
+console.log('firstName');
+
+var lastName = "Estrada"; // Declarar y asignar
+lastName = 'Ana'; // Reasignar
+console.log(lastname);
+
+var secondName = "Javier" // Declarar y asignar
+var secondName = 'Ricardod'; // Reasignar
+console.log(secondName);
+
+// Let, no se puede redeclarar pero si reasignar
+
+let fruit = 'apple' // Declarar y Asignar
+fruit = 'Kiwi'; // Reasigar
+let fruit = 'Banana' 
+
+
+// const, no se puede redeclar ni reasignar
+const animal = 'dog'; // Declara y asigna
+animal = 'hola'; // Reasignar
+console.log(animal);
+
+const vehicles = [];
+
+vehicles.push("Carrito");
+console.log(vehicles)
+
+vehicles.pop()
+console.log(vehicles)
