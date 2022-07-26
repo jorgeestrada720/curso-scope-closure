ef1a150cf114c93f33c72ee1c8b3a75e24dc0733
Jorge Estrada <ejorge720@gmail.com>
2022-07-26 09:56:31 -0600

    Hoisting

---
+++ src/closure/index.js
@@ -0,0 +1,16 @@
+
+
+// var nameOfDog; // Undefined
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+
+var elmo; // Undefined
+nameofDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
